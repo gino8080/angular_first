989ce1ea49e5c53de87b2caae451ffd6e6d7f02d
gino8080 <gino8080@gmail.com>
2019-01-18 09:36:34 +0100

    router setup first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitoloComponent } from './titolo/titolo.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { CocktailListComponent } from './cocktail-list/cocktail-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitoloComponent,
+    TopbarComponent,
+    FooterComponent,
+    LoginComponent,
+    CocktailListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
